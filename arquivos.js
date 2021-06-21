64fd5d5bc9679dc4d1b923be1d2573a8995a39da
Almir Junior <almir.junior@triasoftware.com.br>
2021-06-21 18:20:22 -0300

    first commit

--- arquivos.js
+++ arquivos.js
@@ -1,1 +1,8 @@
 console.log('Adicionei o arquivo dados/arluza.txt')
+
+function returnName(firstName) {
+    return console.log('Olá ' + firstName);
+}
+
+// o nome deve ser passado entre aspas pois é uma string
+returnName('almir'); 
